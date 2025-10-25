8c4624c24eb9193edb7fcccf2ede24f392ecdb00
Dixith Adithya <31dixithadithya@gmail.com>
2025-10-25 15:04:21 +0530

    Hide staging directory: Add staging/ to .gitignore to keep automation files hidden

--- staging/_staging/beginner/odd_even.py
+++
@@ -1,9 +0,0 @@
-def check_odd_even(number):
-    if number % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
-
-number = 7
-result = check_odd_even(number)
-print(f"{number} is {result}")
