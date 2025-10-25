64e833978960c298287005e3c6a534c562e6b046
Dixith Adithya <31dixithadithya@gmail.com>
2025-10-25 15:18:40 +0530

    Hide files directory: Add files/ to .gitignore to keep automation files hidden

--- files/_staging/intermediate/multiplication_table_number.py
+++
@@ -1,8 +0,0 @@
-def print_multiplication_table_of_number(number, limit):
-    print(f"Multiplication table of {number}:")
-    for i in range(1, limit + 1):
-        print(f"{number} x {i} = {number * i}")
-
-number = 7
-limit = 10
-print_multiplication_table_of_number(number, limit)
